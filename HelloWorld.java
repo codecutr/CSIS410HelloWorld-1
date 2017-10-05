c745485319438643b3321817a1a1c436eebefda8
MikeSalvador <ms31salv@siena.edu>
2017-10-05 17:31:09 -0400

    Phooey

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Main
+     */
+    public Main()
+    {
+        System.out.println("Hello World!!!!!!!!");
+    }
+
+   
+}
